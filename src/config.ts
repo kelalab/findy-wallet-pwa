ee31e16f877eac83cacb3258b6b4e60c236093d0
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2021-11-26 09:31:06 +0200

    test .env file

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,7 @@
-let gqlHost;
-let authHost;
-let httpScheme = 'http';
-let wsScheme = 'ws';
-if(process.env.NODE_ENV === "development"){
-  gqlHost = process.env.REACT_APP_GQL_HOST || 'localhost:8085'
-  authHost = process.env.REACT_APP_AUTH_HOST || 'localhost:8088'
-  httpScheme = process.env.REACT_APP_HTTP_SCHEME || 'http'
-  wsScheme = process.env.REACT_APP_WS_SCHEME || 'ws'
-} else {
-  if(process.env.REACT_APP_GQL_HOST){
-    gqlHost = process.env.REACT_APP_GQL_HOST;
-  }
-  if(process.env.REACT_APP_AUTH_HOST){
-    authHost = process.env.REACT_APP_AUTH_HOST;
-  }
-  if(process.env.REACT_APP_HTTP_SCHEME){
-    httpScheme = process.env.REACT_APP_HTTP_SCHEME;
-  }
-  if(process.env.REACT_APP_WS_SCHEME){
-    wsScheme = process.env.REACT_APP_WS_SCHEME;
-  }
-}
+const gqlHost = process.env.REACT_APP_GQL_HOST || 'localhost:8085'
+const authHost = process.env.REACT_APP_AUTH_HOST || 'localhost:8088'
+const httpScheme = process.env.REACT_APP_HTTP_SCHEME || 'http'
+const wsScheme = process.env.REACT_APP_WS_SCHEME || 'ws'
 
 const config = {
   wsUrl: `${wsScheme}://${gqlHost}`,
