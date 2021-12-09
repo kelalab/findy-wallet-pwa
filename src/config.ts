a0d3d69f4aa4309d5f8b94a8e98527a969487ceb
Mika Lindelä <mika.lindela@kelalab.onmicrosoft.com>
2021-12-09 13:56:39 +0200

    testing

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const authHost = process.env.REACT_APP_AUTH_HOST || 'localhost:8088'
 //const wsScheme = process.env.REACT_APP_WS_SCHEME || 'ws'
 
 const wsHost = (gqlHost.indexOf(':') === -1)? window.location.host+gqlHost : gqlHost.replace(/http(s)?:\/\//, '');
-const wsScheme = gqlHost.indexOf('https') === -1? 'ws' : 'wss';
+const wsScheme = window.location.href.indexOf('https') === -1? 'ws' : 'wss';
 
 const config = {
   wsUrl: `${wsScheme}://${wsHost}`,
